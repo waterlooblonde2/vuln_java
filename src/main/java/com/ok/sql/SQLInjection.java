17ea4beb384892b4dba9b96ad918262ba948abb4
waterlooblonde2 <99832836+waterlooblonde2@users.noreply.github.com>
2022-02-23 12:58:42 -0500

    Update SQLInjection.java

--- src/main/java/com/ok/sql/SQLInjection.java
+++ src/main/java/com/ok/sql/SQLInjection.java
@@ -35,4 +35,19 @@ public class SQLInjection {
     @SuppressWarnings("unused")
     ResultSet rs = c.createStatement().executeQuery(sql);
   }
+  
+  public void anotherUnsafeFindProductsByCustomerId(String customerId) throws SQLException {
+    //method 3
+    DataSource dataSource = null;
+    
+    String sql = "select "
+      + "product_id,acc_number,branch_id,balance "
+      + "from Products where customer_id = '"
+      + customerId 
+      + "'";
+    @SuppressWarnings("null")
+    Connection c = dataSource.getConnection();
+    @SuppressWarnings("unused")
+    ResultSet rs = c.createStatement().executeQuery(sql);
+  }
 }
